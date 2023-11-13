bae65949a39c1df13a8c4448e6a1c83b50c7ead1
MrAladawee <91622707+MrAladawee@users.noreply.github.com>
2023-11-13 20:50:24 +0300

    lagrange graph

--- main.kt
+++ main.kt
@@ -28,6 +28,54 @@ import androidx.compose.ui.window.application
 import java.awt.MouseInfo
 import java.awt.Point
 import java.awt.event.MouseEvent
+import kotlin.Exception
+
+fun Interpolation(Ox : Array<Double>, Oy : Array<Double>, xval : Double) : Double
+{
+
+    /*
+    yval = sum(i = 0, Ox.size - 1) Oy[i] * ( prod(j = 0, Ox.size - 1) if (i!=j) (xval - Ox[j])/(Ox[i] - Ox[j]) )
+     */
+
+    var yval = 0.0;
+    var prod : Double
+
+    for (i in 0 .. Ox.size - 1)
+    {
+        prod = Oy[i]
+
+        for (j in 0 .. Ox.size - 1)
+        {
+            if (i != j)
+            {
+                prod *= (xval - Ox[j]) / (Ox[i] - Ox[j])
+            }
+        }
+        yval += prod
+    }
+    return yval
+}
+
+// Общее вычисление значений Oy во всем множестве точек
+fun Interpolation(Ox : Array<Double>, Oy : Array<Double>, xvals : Array<Int>) : Array<Double>
+{
+    var yvals = Array(xvals.size) { 0.0 }
+
+    for (i in 0 .. xvals.size - 1) {
+        yvals[i] = Interpolation(Ox, Oy, xvals[i].toDouble())
+    }
+
+    return yvals;
+}
+
+
+fun convertToScreen(cartesianx : Double, cartesiany : Double, screenwidth : Int, screenheight : Int) : Pair<Double, Double> {
+    return Pair(cartesianx + screenwidth / 2, -cartesiany + screenheight / 2)
+}
+
+fun converToDec(screenx : Double, screeny : Double, screenwidth : Int, screenheight : Int) : Pair<Double, Double>{
+    return Pair(screenx - screenwidth / 2, -screeny + screenheight / 2)
+}
 
 
 @OptIn(ExperimentalTextApi::class, ExperimentalComposeUiApi::class)
@@ -37,8 +85,10 @@ fun App() {
 
     // Initialization values of Mins & Maxs x, y
     var xMin by remember { mutableStateOf(-10) }
-    var yMin by remember { mutableStateOf(0) }
     var xMax by remember { mutableStateOf( 10) }
+    var yMin by remember { mutableStateOf(0) }
+    var ymin by remember { mutableStateOf(-10) }
+    var yMax by remember { mutableStateOf(10) }
 
     var yMinMax by remember{mutableStateOf(0f)}
     var selectY by remember{mutableStateOf(true)}
@@ -46,17 +96,27 @@ fun App() {
     val textMeasurer = rememberTextMeasurer()
 
     // DataBase of dots. Example: (0.0, 1.0)
-    var points by remember { mutableStateOf(mutableMapOf<Float, Float>()) }
+    var points_screen by remember { mutableStateOf(mutableMapOf<Double, Double>()) }
+    var points_cart by remember { mutableStateOf(mutableMapOf<Double, Double>()) }
+
 
     // Main window for graphics
     Canvas(modifier = Modifier.fillMaxSize().clickable{}. onPointerEvent(PointerEventType.Press){
 
+
         // Click in window
-        var point = it.changes.first().position
-        points[point.x] = point.y
+        var point = it.changes.first().position // screen point
+
+        var (point_dec_x, point_dec_y) = converToDec(point.x.toDouble(), point.y.toDouble(), this.size.width, this.size.height)
+
+        points_cart[point_dec_x] = point_dec_y
+        points_screen[point.x.toDouble()] = point.y.toDouble()
 
     },
         onDraw = {
+
+            var width = this.size.width.toInt()
+            var height = this.size.height.toInt()
 
             var yMax = this.size.height*(xMax-xMin)/this.size.width + yMin
 
@@ -76,14 +136,6 @@ fun App() {
                 start = Offset(-this.size.width*xMin/(xMax-xMin), 0f),
                 end = Offset(-this.size.width*xMin/(xMax-xMin), this.size.height))
 
-            for(point in points) {
-                drawCircle(
-                    color = Color.Green,
-                    radius = 10f,
-                    center = Offset(point.key, point.value)
-                )
-            }
-
             // Text below OX
             for(i in xMin .. xMax) {
 
@@ -96,8 +148,108 @@ fun App() {
                 drawText(textMeasurer = textMeasurer, text = i.toString(),
                     topLeft = Offset(this.size.width*(i-xMin)/(xMax-xMin)-5,
                         this.size.height*(1+yMinMax)/2))
+
             }
 
+            /*
+                for(point in points) {
+                drawCircle(
+                    color = Color.Green,
+                    radius = 10f,
+                    center = Offset(point.key.toFloat(), point.value.toFloat())
+                )
+            }
+            */
+
+
+            for (point in points_cart) {
+
+                var (pointScreeX, pointScreenY) = convertToScreen(
+                    point.key.toDouble(),
+                    point.value.toDouble(),
+                    width,
+                    height
+                )
+
+                drawCircle(
+                    color = Color.Red,
+                    radius = 10f,
+                    center = Offset(pointScreeX.toFloat(), pointScreenY.toFloat())
+                )
+            }
+
+            // Узловые points
+            var Ox_screen: Array<Double> = Array(points_screen.size) { 0.0 }
+            var Oy_screen: Array<Double> = Array(points_screen.size) { 0.0 }
+
+            var count = 0
+            for (point in points_screen) {
+                Ox_screen[count] = point.key
+                Oy_screen[count] = point.value
+                count++
+            }
+
+            // All pixels
+            var xvals_screen: Array<Int> = Array(width) { 0 }
+            for (i in 0..width - 1) {
+                xvals_screen[i] = i
+            }
+
+            var yvals_screen: Array<Double>
+            yvals_screen = Interpolation(Ox_screen, Oy_screen, xvals_screen)
+
+            for (i in 1..xvals_screen.size - 1) {
+                drawLine(
+                    color = Color.Blue,
+                    start = Offset(xvals_screen[i - 1].toFloat(), yvals_screen[i - 1].toFloat()),
+                    end = Offset(xvals_screen[i].toFloat(), yvals_screen[i].toFloat())
+                )
+            }
+
+
+            /*if (points.size != 0) {
+                var coord = Array<Point>(this.size.width.toInt()) {Point(0,0)}
+
+                var xvals = Array(this.size.width.toInt()) {0};
+                var x : Array<Double> = Array(points.size) {0.0}
+                var y : Array<Double> = Array(points.size) {0.0}
+
+                var count = 0
+
+                for (part in points) {
+                    drawCircle(
+                        color = Color.Green,
+                        radius = 10f,
+                        center = Offset(part.key.toFloat(), part.value.toFloat())
+                    )
+
+                    x[count] = part.key.toDouble()
+                    y[count] = part.key.toDouble()
+
+                    count++
+                }
+
+                for (i in 0 .. this.size.width.toInt() - 1) {
+                    xvals[i] = i
+                }
+
+                var yvals = Interpolation(x,y, xvals)
+
+                for (i in 0 .. points.size - 1) {
+                    coord[i].x = xvals[i]
+                    coord[i].y = yvals[i].toInt()
+                }
+
+                for (i in 1..coord.size-1) {
+                    drawLine(
+                        color = Color.Blue,
+                        start = Offset(coord[i-1].x.toFloat(), coord[i-1].y.toFloat()),
+                        end = Offset(coord[i].x.toFloat(), coord[i].y.toFloat())
+                    )
+                }
+
+            }
+             */
         })
 
     // Text and edit values of Mins & Maxs x,y
