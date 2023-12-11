4cf343ae7c2dc549bcb934580ee32d81fbec1d45
MrAladawee <91622707+MrAladawee@users.noreply.github.com>
2023-12-11 21:24:22 +0300

    delete scroll yRescale & reedit converts

--- main.kt
+++ main.kt
@@ -81,20 +81,20 @@ fun Interpolation(Ox : Array<Double>, Oy : Array<Double>, xvals : Array<Double>)
     return yvals;
 }
 
-fun convertToScreen(cartesianx : Double, cartesiany : Double, screenwidth : Int, screenheight : Int) : Pair<Double, Double> {
-    return Pair(cartesianx + screenwidth / 2, -cartesiany + screenheight / 2)
+fun convertToScreen(cartesianx : Double, cartesiany : Double, screenwidth : Int, screenheight : Int, xMax : Double, xMin : Double, yMax: Double, yMin: Double) : Pair<Double, Double> {
+    var x_ = (((cartesianx - xMin) * screenwidth) / (xMax - xMin))
+    var y_ = (((yMax - cartesiany) * screenheight) / (yMax - yMin))
+    return Pair(x_, y_)
 }
 
-fun converToDec(screenx : Double, screeny : Double, screenwidth : Int, screenheight : Int) : Pair<Double, Double>{
-    return Pair(screenx - screenwidth / 2, -screeny + screenheight / 2)
+fun converToDec(screenx : Double, screeny : Double, screenwidth : Int, screenheight : Int, xMax : Double, xMin : Double, yMax: Double, yMin: Double) : Pair<Double, Double>{
+    var x_ = screenx * (xMax-xMin) / screenwidth + xMin
+    var y_ = yMax - screeny*(yMax-yMin) / screenheight
+    return Pair(x_, y_)
 }
 
-fun converToDecX(screenx : Double, screenwidth : Int, screenheight : Int) : Double{
-    return screenx - screenwidth / 2
-}
-
-fun converToDecY(screeny : Double, screenwidth : Int, screenheight : Int) : Double{
-    return -screeny + screenheight / 2
+fun converToDecX(screenx : Double, screenwidth : Int, screenheight : Int, xMax : Double, xMin : Double) : Double{
+    return screenx*(xMax-xMin) / screenwidth + xMin
 }
 
 var DataSetScale : Array<Pair<Int,Int>> = Array<Pair<Int,Int>>(2) {Pair(0,0)}
@@ -105,14 +105,13 @@ var DataSetScale : Array<Pair<Int,Int>> = Array<Pair<Int,Int>>(2) {Pair(0,0)}
 fun App() {
 
     // Initialization values of Mins & Maxs x, y
-    var xMin by remember { mutableStateOf(-10) }
-    var xMax by remember { mutableStateOf( 10) }
-    var yMin by remember { mutableStateOf(0) }
-    var ymin by remember { mutableStateOf(-10) }
-    var yMax by remember { mutableStateOf(10) }
+    var xMin by remember { mutableStateOf(-10.0) }
+    var xMax by remember { mutableStateOf( 10.0) }
+    var yMin by remember { mutableStateOf(0.0) }
+    var ymin by remember { mutableStateOf(-10.0) }
+    var yMax by remember { mutableStateOf(10.0) }
 
     var yMinMax by remember{mutableStateOf(0f)}
-    var selectY by remember{mutableStateOf(true)}
 
     val textMeasurer = rememberTextMeasurer()
 
@@ -128,8 +127,9 @@ fun App() {
         // Click in window
         var point = it.changes.first().position // screen point
 
-        var (point_dec_x, point_dec_y) = converToDec(point.x.toDouble(), point.y.toDouble(), this.size.width, this.size.height)
+        var (point_dec_x, point_dec_y) = converToDec(point.x.toDouble(), point.y.toDouble(), this.size.width, this.size.height, xMax, xMin, yMax, ymin)
 
+        println("$point_dec_x, $point_dec_y")
 
         points_cart[point_dec_x] = point_dec_y
         points_screen[point.x.toDouble()] = point.y.toDouble()
@@ -139,67 +139,43 @@ fun App() {
 
             var width : Int
             var height : Int
+            width = this.size.width.toInt()
+            height = this.size.height.toInt()
 
-            if (DataSetScale[0] == Pair(0,0)) {
-                width = this.size.width.toInt()
-                height = this.size.height.toInt()
-                DataSetScale[0] = Pair(width,height)
-            }
-            else if (DataSetScale[0] != Pair(0,0) && DataSetScale[1] == Pair(0,0)) {
-                width = this.size.width.toInt()
-                height = this.size.height.toInt()
-                DataSetScale[1] = Pair(width,height)
-            }
-            else {
-                DataSetScale[0] = DataSetScale[1]
-                width = this.size.width.toInt()
-                height = this.size.height.toInt()
-                DataSetScale[1] = Pair(width,height)
-            }
-
-            var yMax = this.size.height*(xMax-xMin)/this.size.width + yMin
-
-            if(yMax*yMin<0){
+            if(yMax*yMin<0) {
                 yMinMax = ((xMax-xMin)/(xMax+xMin)).toFloat()
             }
+            println(yMinMax)
 
             // Axis OX
             drawLine(
                 color = Color.Black,
-                start = Offset(0f, this.size.height * (1 + yMinMax) / 2),
-                end = Offset(this.size.width, this.size.height * (1 + yMinMax) / 2)
+                start = Offset(0f, (this.size.height * (0 - xMin) / (xMax - xMin)).toFloat()),
+                end = Offset(this.size.width, (this.size.height * (0 - xMin) / (xMax - xMin)).toFloat())
             )
 
             // Axis OY
             drawLine(color = Color.Black,
-                start = Offset(-this.size.width*xMin/(xMax-xMin), 0f),
-                end = Offset(-this.size.width*xMin/(xMax-xMin), this.size.height))
+                start = Offset((-this.size.width*xMin/(xMax-xMin)).toFloat(), 0f),
+                end = Offset((-this.size.width*xMin/(xMax-xMin)).toFloat(), this.size.height))
 
             // Text below OX
-            for(i in xMin .. xMax) {
+            for(i in xMin.toInt() ..xMax.toInt()) {
 
                 drawLine(color = Color.Black,
-                    start = Offset(this.size.width*(i-xMin)/(xMax-xMin),
+                    start = Offset(
+                        (this.size.width*(i-xMin)/(xMax-xMin)).toFloat(),
                         this.size.height*(1+yMinMax)/2-5),
-                    end = Offset(this.size.width*(i-xMin)/(xMax-xMin),
+                    end = Offset(
+                        (this.size.width*(i-xMin)/(xMax-xMin)).toFloat(),
                         this.size.height*(1+yMinMax)/2+5))
 
                 drawText(textMeasurer = textMeasurer, text = i.toString(),
-                    topLeft = Offset(this.size.width*(i-xMin)/(xMax-xMin)-5,
+                    topLeft = Offset(
+                        (this.size.width*(i-xMin)/(xMax-xMin)-5).toFloat(),
                         this.size.height*(1+yMinMax)/2))
 
             }
-
-            /*
-                for(point in points) {
-                drawCircle(
-                    color = Color.Green,
-                    radius = 10f,
-                    center = Offset(point.key.toFloat(), point.value.toFloat())
-                )
-            }
-            */
-
 
             for (point in points_cart) {
 
@@ -207,7 +183,8 @@ fun App() {
                     point.key.toDouble(),
                     point.value.toDouble(),
                     width,
-                    height
+                    height,
+                    xMax, xMin, yMax, ymin
                 )
 
                 drawCircle(
@@ -216,36 +193,6 @@ fun App() {
                     center = Offset(pointScreeX.toFloat(), pointScreenY.toFloat())
                 )
             }
-
-            /* РАБОТА С СКРИН ПОИНТ
-            // Узловые points
-            var Ox_screen: Array<Double> = Array(points_screen.size) { 0.0 }
-            var Oy_screen: Array<Double> = Array(points_screen.size) { 0.0 }
-
-            var count = 0
-            for (point in points_screen) {
-                Ox_screen[count] = point.key
-                Oy_screen[count] = point.value
-                count++
-            }
-
-            // All pixels
-            var xvals_screen: Array<Int> = Array(width) { 0 }
-            for (i in 0..width - 1) {
-                xvals_screen[i] = i
-            }
-
-            var yvals_screen: Array<Double>
-            yvals_screen = Interpolation(Ox_screen, Oy_screen, xvals_screen)
-
-            for (i in 1..xvals_screen.size - 1) {
-                drawLine(
-                    color = Color.Blue,
-                    start = Offset(xvals_screen[i - 1].toFloat(), yvals_screen[i - 1].toFloat()),
-                    end = Offset(xvals_screen[i].toFloat(), yvals_screen[i].toFloat())
-                )
-            }
-            */
 
             // Узловые points
             var Ox_cart: Array<Double> = Array(points_screen.size) { 0.0 }
@@ -264,7 +211,7 @@ fun App() {
 
             for (i in 0..width-1) {
 
-                xvals_cart[i] = converToDecX(i.toDouble(), width, height)
+                xvals_cart[i] = converToDecX(i.toDouble(), width, height, xMax, xMin)
 
             }
 
@@ -274,8 +221,8 @@ fun App() {
             for (i in 1..xvals_cart.size - 1) {
 
 
-                var (first_x_screen, frist_y_screen) = convertToScreen(xvals_cart[i - 1], yvals_cart[i - 1], width, height)
-                var (second_x_screen, second_y_screen) = convertToScreen(xvals_cart[i], yvals_cart[i], width, height)
+                var (first_x_screen, frist_y_screen) = convertToScreen(xvals_cart[i - 1], yvals_cart[i - 1], width, height, xMax, xMin, yMax, ymin)
+                var (second_x_screen, second_y_screen) = convertToScreen(xvals_cart[i], yvals_cart[i], width, height, xMax, xMin, yMax, ymin)
 
 
 
@@ -285,50 +232,6 @@ fun App() {
                     end = Offset(second_x_screen.toFloat(), second_y_screen.toFloat())
                 )
             }
-
-            /*if (points.size != 0) {
-                var coord = Array<Point>(this.size.width.toInt()) {Point(0,0)}
-
-                var xvals = Array(this.size.width.toInt()) {0};
-                var x : Array<Double> = Array(points.size) {0.0}
-                var y : Array<Double> = Array(points.size) {0.0}
-
-                var count = 0
-
-                for (part in points) {
-                    drawCircle(
-                        color = Color.Green,
-                        radius = 10f,
-                        center = Offset(part.key.toFloat(), part.value.toFloat())
-                    )
-
-                    x[count] = part.key.toDouble()
-                    y[count] = part.key.toDouble()
-
-                    count++
-                }
-
-                for (i in 0 .. this.size.width.toInt() - 1) {
-                    xvals[i] = i
-                }
-
-                var yvals = Interpolation(x,y, xvals)
-
-                for (i in 0 .. points.size - 1) {
-                    coord[i].x = xvals[i]
-                    coord[i].y = yvals[i].toInt()
-                }
-
-                for (i in 1..coord.size-1) {
-                    drawLine(
-                        color = Color.Blue,
-                        start = Offset(coord[i-1].x.toFloat(), coord[i-1].y.toFloat()),
-                        end = Offset(coord[i].x.toFloat(), coord[i].y.toFloat())
-                    )
-                }
-
-            }
-             */
 
         })
 
@@ -342,7 +245,7 @@ fun App() {
 
                 Text("xMin")
                 TextField(value = xMin.toString(),
-                    onValueChange = { value -> xMin = value.toIntOrNull() ?:-10 })
+                    onValueChange = { value -> xMin = value.toDoubleOrNull() ?: 0.0 })
 
             }
 
@@ -351,48 +254,26 @@ fun App() {
 
                 Text("xMax")
                 TextField(value = xMax.toString(),
-                    onValueChange = { value -> xMax = value.toIntOrNull() ?: 0 })
+                    onValueChange = { value -> xMax = value.toDoubleOrNull() ?: 0.0 })
 
             }
 
-            // Select input type for yMin & yMinMax
-            Column{
-
-                Row{
-                    RadioButton(
-                        selected = selectY,
-                        onClick = { selectY = true },
-                        modifier = Modifier.padding(8.dp)
-                    )
-                    Text("yMin", fontSize = 22.sp)
-                }
-                Row{
-                    RadioButton(
-                        selected = !selectY,
-                        onClick = { selectY = false },
-                    )
-                    Text("ySlider", fontSize = 22.sp)
-                }
-
-            }
-
+            // TextField for yMax
             Column(modifier = Modifier.padding(10.dp, 10.dp)) {
 
-                // Hide slider for weight
-                if(!selectY)
-                {
-                    Slider(value = yMinMax,
-                        valueRange = -1f..1f,
-                        steps = 9,
-                        onValueChange = { yMinMax = it })
-                }
+                Text("yMax")
+                TextField(value = yMax.toString(),
+                    onValueChange = { value -> yMax = value.toDoubleOrNull() ?: 0.0 })
 
-                // Hide textField for yMin
-                else
-                {
-                    TextField(value = yMin.toString(),
-                        onValueChange = { value -> yMin = value.toIntOrNull() ?: 0 })
-                }
+            }
+
+            // TextField for yMin
+            Column(modifier = Modifier.padding(10.dp, 10.dp)) {
+
+                Text("yMin")
+                TextField(value = ymin.toString(),
+                    onValueChange = { value -> ymin = value.toDoubleOrNull() ?: 0.0 })
+
             }
         }
     }
